61eb7270411dc723405f99db275d768ac7182166
gilbertfoust <gilbert.foust@humanitypathwaysglobal.com>
2026-06-16 12:24:19 -0400

    Add data health panel to dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -33,6 +33,7 @@ import { format, subMonths, startOfMonth } from "date-fns";
 import { TodaysActionCenter } from "@/components/dashboard/TodaysActionCenter";
 import { ModuleSnapshotCards } from "@/components/dashboard/ModuleSnapshotCards";
 import { RecentActivityFeed } from "@/components/dashboard/RecentActivityFeed";
+import { DataHealthPanel } from "@/components/dashboard/DataHealthPanel";
 
 const HPG_LOGO_URL =
   "https://img1.wsimg.com/isteam/ip/8d5502d6-d937-4d80-bd56-8074053e4d77/Humanity%20Pathways%20Global.jpg/:/rs=h:175,m";
@@ -417,6 +418,9 @@ const Dashboard = () => {
 
       {/* Recent Activity */}
       <RecentActivityFeed />
+
+      {/* Data Health */}
+      <DataHealthPanel />
 
       {/* Quick Navigation */}
       <QuickNavCards />
